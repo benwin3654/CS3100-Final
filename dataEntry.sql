d446ffe58e1a6c86b408725282671e6e84381ba7
gonzalez-america <america.gonzalez12@outlook.com>
2024-12-03 00:53:50 -0600

    Update dataEntry.sql

--- dataEntry.sql
+++ dataEntry.sql
@@ -1,4 +1,5 @@
--- ALL EMPLOYEES ENTERED ********	
+          
+-- ALL EMPLOYEES ENTERED		
 INSERT INTO employees
 	(employeeID, firstName, lastName, 
     gender, title, streetAddress, 
@@ -62,49 +63,49 @@ INSERT INTO employees
     '1651 Naismith Drive', 'Lawrence', 'KS', '66044',
     '9130123344', '1001');
 
--- PRODUCTS DATA ENTRY*******
+-- PRODUCTS DATA ENTRY
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES ('112', '5 1/4 Floppy Disks', '0.75');
+	(partID, productDescription)
+    VALUES ('112', '5 1/4 Floppy Disks');
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('113', '3 1/2 Disks', '1.25');
+	(partID, productDescription)
+    VALUES('113', '3 1/2 Disks');
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('221', 'Phone Support Hourly', '75.00');
+	(partID, productDescription)
+    VALUES('221', 'Phone Support Hourly');
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('222', 'Phone Support (Family) Hourly', '225.00');
+	(partID, productDescription)
+    VALUES('222', 'Phone Support (Family) Hourly');
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('337', 'User Encouragement Device (Electric)', '74.00');
+	(partID, productDescription)
+    VALUES('337', 'User Encouragement Device (Electric)');
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('338', 'User Encouragement Device - Club Style (Black)', '52.00');
+	(partID, productDescription)
+    VALUES('338', 'User Encouragement Device - Club Style (Black)');
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('339', 'User Encouragement Device - Club Style (Red)', '60.00');
+	(partID, productDescription)
+    VALUES('339', 'User Encouragement Device - Club Style (Red)');
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('443', 'Really Fine User Manual', '327.00');
+	(partID, productDescription)
+    VALUES('443', 'Really Fine User Manual');
     
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('445', 'Motivational Poster (Framed)', '17.00');
+	(partID, productDescription)
+    VALUES('445', 'Motivational Poster (Framed)');
 
 INSERT INTO products
-	(partID, productDescription, priceEach)
-    VALUES('1007', 'User Tolerance Assistance', '3.14');
+	(partID, productDescription)
+    VALUES('1007', 'User Tolerance Assistance');
 
--- ORDERS DATA ENTRY********
+-- ORDERS DATA ENTRY
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
@@ -116,7 +117,7 @@ INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
     VALUES('1002', '2376', '2024-11-02', '1041', '1007',
-    '2024-12-02', 'EMPTY', 'EMPTY'); -- check
+    '2024-12-02'); -- Phone Assistance
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
@@ -140,13 +141,13 @@ INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
     VALUES('1006', '3724', '2024-11-18', '1032', '1007', 
-    '2024-12-18', '2024-11-30', 'EMPTY');
+    '2024-12-18', '2024-11-30');
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
     VALUES('1007', '1728', '2024-11-20', '1064', '1004', 
-    '2024-12-20', 'EMPTY', 'EMPTY');
+    '2024-12-20'); -- Phone assistance
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
@@ -158,68 +159,69 @@ INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
     VALUES('1009', '1003', '2024-11-28', '1091', '1006', 
-    '2024-12-28', '2024-12-12', '1006'); 
+    '2024-12-28', '2024-12-12', '1006');
 
--- ORDER DETAILS DATA ENTRY**********
-
-INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1001','112','7');
+-- CHECK THE DISCOUNTS AGAIN!!!
+-- ORDER DETAILS DATA ENGTRY 
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1001', '221', '1');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1001','112','7', '2.75');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1001','339','12');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1001', '221', '1', '2.75');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1001','339','12', '2.75');
+
+INSERT INTO orderDetails
+	(orderID, partID, quantity)
     VALUES('1002', '222', '5');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1003', '443', '5');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1003', '443', '5', '25.00');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1003', '445', '12');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1003', '445', '12', '25.00');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1003', '107', '50');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1003', '107', '50', '25.00');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1004', '221', '2');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1004', '221', '2', '32.00');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1004', '222', '5');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1004', '222', '5', '32.00');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1004', '337', '1');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1004', '337', '1', '32.00');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1004', '338', '3');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1004', '338', '3', '32.00');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1005', '112', '501');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1005', '112', '501', '12.25');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1005', '113', '125');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1005', '113', '125', '12.25');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
+	(orderID, partID, quantity)
     VALUES('1006', '443', '23');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
+	(orderID, partID, quantity)
     VALUES('1006', '337', '4');
 
 INSERT INTO orderDetails
@@ -227,104 +229,106 @@ INSERT INTO orderDetails
     VALUES('1006', '107', '44000', '0.1');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
+	(orderID, partID, quantity)
     VALUES('1007', '221', '12');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1008', '443', '10');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1008', '443', '10', '32.50');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1008', '445', '5');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1008', '445', '5', '32.50');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1009', '112', '50');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1009', '112', '50', '12.75');
 
 INSERT INTO orderDetails
-	(orderID, partID, quantity, discount)
-    VALUES('1009', '113', '150');
+	(orderID, partID, quantity, shippingPrice)
+    VALUES('1009', '113', '150', '12.75');
+    
 
--- CONTACTS DATA ENTRY*******
+-- CONTACTS DATA ENTRY
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1001', 'Patricia', 'Smith', '1122', 
     '111 Santa Anna Valley Road', 'Sacramento', 'CA', '94203', '9161014443', '1');
     
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1002', 'Michael', 'Williams', '1122', 
     '1066 Gayley Ave', 'Los Angeles', 'CA', '90024', '2131108590', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1003', 'Michael', 'Jones', '1122',
     '5101 York Blvd', 'Los Angeles', 'CA', '90042', '2131110003', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1004', 'John', 'Wilson', '1122', 
     '5101 York Blvd', 'Los Angeles', 'CA', '90042', '2131110003', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1031', 'Mary', 'Parsons', '3724', 
     '775 Normal Lane', 'Relational', 'KS', '66110', '9139998877', '1');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1032', 'Larry', 'DBA', '3724', 
     '237 Boyce Avenue', 'Codd', 'KS', '67444', '9137870000', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1033', 'Thomas', 'Taylor', '3724',
     '738 SW Gage Blvd', 'Topeka', 'KS', '66606', '9131988567', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1034', 'Linda', 'Wilson', '3724',
     '4745 NW Hunters Ridge Cir', 'Topeka', 'KS', '66618', '9131012409', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1035', 'Richard', 'Garcia', '3724', 
     '16563 W 151st St', 'Oleathe', 'KS', '66206', '9131592910', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1036', 'Joseph', 'Martin', '3724',
     '7732 E Central Ave', 'Whichita', 'KS', '67206', '9131386984', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1037', 'Joseph', 'Thomas', '3724', 
     '7002 Johnson Dr', 'Mission', 'KS', '66202', '9131812280', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1038', 'Daniel', 'Lee', '3724', '9474',
     '9474 Renner Blvd', 'Lanexa', 'KS', '6619', '9131755459', '0');
 
 INSERT INTO contacts
 	(contactID, contactFirstName, contactLastName, customerID,
-    address, city, zipCode, phoneNumber, isBilling)
+    address, city, state, zipCode, phoneNumber, isBilling)
     VALUES('1039', 'Jennifer', 'Taylor', '3724',
     '150 Central Mall', 'Salina', 'KS', '57401', '9131132430', '0');
+
 
 -- CUSTOMERS DATA ENTRY
 
@@ -423,27 +427,27 @@ INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001', 'AL');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','AK');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','AZ');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','CT');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','DE');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','FL');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','GA');
 
@@ -455,166 +459,234 @@ INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','ID');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','KY');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','LA');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','ME');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','MD');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','MA');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','MI');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','MS');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','MT');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','NV');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','NH');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','NJ');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','NY');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','NC');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','ND');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','OR');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','PA');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','RI');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','SC');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','SD');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','UT');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','VT');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','VA');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','WA');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','WV');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1001','WY');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1002','CO');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1002','NM');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1002','TX');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1003','CA');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1004','IL');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1004','OH');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1004','IN');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1005','AR');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1005','OK');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1005','TN');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1006','IA');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1006','WI');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1006','MN');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1007','MO');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1007','KS');
     
-    INSERT INTO regions
+INSERT INTO regions
 	(employeeID, state)
 	VALUES('1007','NE');
+    
+-- Price Table
+
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('112', '0.75', '2024-11-01');
+
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('113', '1.25', '2024-11-01');
+
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('221', '75.00', '2024-11-01');
+
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('222', '225.00', '2024-11-01');
+    
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('337', '74.00', '2024-11-01');
+    
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('338', '52.00', '2024-11-01');
+
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('339', '60.00', '2024-11-01');
+
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('443', '327.00', '2024-11-01');
+
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('445', '17.50', '2024-11-01');
+    
+INSERT INTO price
+	(partID, priceEah, fromDate, toDate)
+    VALUES('1007', '3.14', '2024-11-01');
+
+-- Shipping Methods
+
+INSERT INTO shippingMethods
+	(methodID, carrier, shippingDesc)
+    VALUES('1001', 'UPS', 'FOB Destination');
+
+INSERT INTO shippingMethods
+	(methodID, carrier, shippingDesc)
+    VALUES('1002', 'USPS', '1st Class');
+
+INSERT INTO shippingMethods
+	(methodID, carrier, shippingDesc)
+    VALUES('1003', 'UPS', 'Next Day');
+
+INSERT INTO shippingMethods
+	(methodID, carrier, shippingDesc)
+    VALUES('1004', 'UPS', 'Standard');
+
+INSERT INTO shippingMethods
+	(methodID, carrier, shippingDesc)
+    VALUES('1005', 'Fedex', 'Overnight');
+    
+INSERT INTO shippingMethods
+	(methodID, carrier, shippingDesc)
+    VALUES('1006', 'UPS', '3 Day, FOB Destination')
