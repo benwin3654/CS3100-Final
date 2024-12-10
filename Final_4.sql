b00017ec232991015fb2e123969d0096750eac78
Ben Winbinger <72219324+benwin3654@users.noreply.github.com>
2024-12-09 22:04:50 -0600

    Updated Code

--- Final_4.sql
+++ Final_4.sql
@@ -21,7 +21,6 @@ CREATE TABLE `employees`(
     `phoneNumber` VARCHAR(15) NOT NULL,
     `reportsTo` INT(4) DEFAULT NULL,
     PRIMARY KEY(`employeeID`),#This sets the primary key to employeeID
-    KEY `employeeID`(`employeeID`),
     CONSTRAINT `employeeID1234` FOREIGN KEY(`reportsTo`) REFERENCES `employees` (`employeeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1; #Sets the storage engine and character set
 
@@ -30,16 +29,18 @@ CREATE TABLE `employees`(
 DROP TABLE IF EXISTS customers;#Checks if the table already exists
 #Creates the customers table
 CREATE TABLE `customers`(
-	`customerID` INT(4) NOT NULL,
+	`customerID` INT AUTO_INCREMENT,
+    `publicCustomerID` INT(4) NOT NULL,
     `customerName` VARCHAR(30) NOT NULL,
     PRIMARY KEY(`customerID`)#Sets the primary key to customerID
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+AUTO_INCREMENT = 1000;
 
 DROP TABLE IF EXISTS contacts;
 
 #Creates the contacts table
 CREATE TABLE contacts(
-	`contactID` INT(4) NOT NULL, #contactID has to have a value
+	`contactID` INT(4) NOT NULL AUTO_INCREMENT, #contactID has to have a value
     `contactFirstName` VARCHAR (20) NOT NULL,
     `contactLastName` VARCHAR (20) DEFAULT NULL, #contactLastName doesn't have to have a value as there are some contacts that didn't have a last name
     `customerID` INT(4) NOT NULL,#customerID has to have a value
@@ -50,9 +51,9 @@ CREATE TABLE contacts(
     `phoneNumber` VARCHAR(15) NOT NULL,
     `isBilling` BOOLEAN NOT NULL,
     PRIMARY KEY(`contactID`),#Sets the primary key to contactID
-    KEY `customerID`(`customerID`),
     CONSTRAINT `customerID1234` FOREIGN KEY(`customerID`) REFERENCES `customers`(`customerID`)#Sets the foreign key to customerID and refers to the customers table
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+AUTO_INCREMENT = 1000;
 
 DROP TABLE IF EXISTS regions;
 
@@ -85,7 +86,7 @@ CREATE TABLE shippingMethods(
 DROP TABLE IF EXISTS orders;
 #Creates the orders table that stores all the orders
 CREATE TABLE orders(
-	`orderID` INT(4) NOT NULL,
+	`orderID` INT(4) NOT NULL AUTO_INCREMENT,
     `customerID` INT(4) NOT NULL,
     `orderDate` DATE NOT NULL,
     `contactID` INT(4) NOT NULL,
@@ -98,7 +99,8 @@ CREATE TABLE orders(
     CONSTRAINT `contactID1334` FOREIGN KEY(`contactID`) REFERENCES `contacts`(`contactID`),
     CONSTRAINT `employeeID1236` FOREIGN KEY(`employeeID`) REFERENCES `employees`(`employeeID`),
     CONSTRAINT `methodID1001` FOREIGN KEY(`methodID`) REFERENCES `shippingMethods`(`methodID`)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+)ENGINE=InnoDB DEFAULT CHARSET=latin1
+AUTO_INCREMENT = 1000;
 
 DROP TABLE IF EXISTS orderDetails;
 
@@ -124,6 +126,7 @@ CREATE TABLE price(
     `toDate` DATE DEFAULT NULL,
     CONSTRAINT `partID122` FOREIGN KEY (`partID`) REFERENCES `products`(`partID`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 -- ALL EMPLOYEES ENTERED		
 INSERT INTO employees
@@ -192,185 +195,186 @@ INSERT INTO employees
 -- CONTACTS DATA ENTRY
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1122', 'Bill & Teds PC Repair');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1375', 'Hestons 10 Commands');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1376', 'Kelsons Northtown');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1835', 'PC Loco');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('3724', 'Databases R Us');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('2298', 'Pyramid PC');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('2376', 'Big Freds Mac Emporium');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1876', 'Macs Welcome Here');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('2374', 'PC Master Race');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1313','Pulcinella PC');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1214','Abos workshop');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1275','Cosmic Computer');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1012','AvantisCommodore');    
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1728','Gullivers traveling techs');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1818','Bits & Bytes');    
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1276','Alma PC connection');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1317','Shotgun Shack Mac');
     INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1456','US PC');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1568','Walton Computers');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1513','Algiers applications');
     INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1001','Brickhouse Computers');
 
 INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1002','Hometown Help');
     INSERT INTO customers
-	(customerID, customerName)
+	(publicCustomerID, customerName)
     VALUES('1003','Michaels Dell Emporium');
 
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1001', 'Patricia', 'Smith', '1122', 
+    VALUES('1001', 'Patricia', 'Smith', '1000', 
     '111 Santa Anna Valley Road', 'Sacramento', 'CA', '94203', '9161014443', '1');
     
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1002', 'Michael', 'Williams', '1122', 
+    VALUES('1002', 'Michael', 'Williams', '1000', 
     '1066 Gayley Ave', 'Los Angeles', 'CA', '90024', '2131108590', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1003', 'Michael', 'Jones', '1122',
+    VALUES('1003', 'Michael', 'Jones', '1000',
     '5101 York Blvd', 'Los Angeles', 'CA', '90042', '2131110003', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1004', 'John', 'Wilson', '1122', 
+    VALUES('1004', 'John', 'Wilson', '1000', 
     '5101 York Blvd', 'Los Angeles', 'CA', '90042', '2131110003', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1031', 'Mary', 'Parsons', '3724', 
+    VALUES('1031', 'Mary', 'Parsons', '1004', 
     '775 Normal Lane', 'Relational', 'KS', '66110', '9139998877', '1');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1032', 'Larry', 'DBA', '3724', 
+    VALUES('1032', 'Larry', 'DBA', '1004', 
     '237 Boyce Avenue', 'Codd', 'KS', '67444', '9137870000', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1033', 'Thomas', 'Taylor', '3724',
+    VALUES('1033', 'Thomas', 'Taylor', '1004',
     '738 SW Gage Blvd', 'Topeka', 'KS', '66606', '9131988567', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1034', 'Linda', 'Wilson', '3724',
+    VALUES('1034', 'Linda', 'Wilson', '1004',
     '4745 NW Hunters Ridge Cir', 'Topeka', 'KS', '66618', '9131012409', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1035', 'Richard', 'Garcia', '3724', 
+    VALUES('1035', 'Richard', 'Garcia', '1004', 
     '16563 W 151st St', 'Oleathe', 'KS', '66206', '9131592910', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1036', 'Joseph', 'Martin', '3724',
+    VALUES('1036', 'Joseph', 'Martin', '1004',
     '7732 E Central Ave', 'Whichita', 'KS', '67206', '9131386984', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1037', 'Joseph', 'Thomas', '3724', 
+    VALUES('1037', 'Joseph', 'Thomas', '1004', 
     '7002 Johnson Dr', 'Mission', 'KS', '66202', '9131812280', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1038', 'Daniel', 'Lee', '3724',
+    VALUES('1038', 'Daniel', 'Lee', '1004',
     '9474 Renner Blvd', 'Lanexa', 'KS', '6619', '9131755459', '0');
 
 INSERT INTO contacts
-	(contactID, contactFirstName, contactLastName, customerID,
+    (contactID, contactFirstName, contactLastName, customerID,
     address, city, state, zipCode, phoneNumber, isBilling)
-    VALUES('1039', 'Jennifer', 'Taylor', '3724',
+    VALUES('1039', 'Jennifer', 'Taylor', '1004',
     '150 Central Mall', 'Salina', 'KS', '57401', '9131132430', '0');
     
 INSERT INTO contacts
     (contactID, contactFirstName, contactLastName, customerID, 
     address, city, state, zipCode, phoneNumber, isBilling) 
 VALUES
-	('1007', 'Maria', 'Rodriguez', '1122', '6541 Hollywood Blvd', 'Los Angeles', 'CA', '90028',	'2131092912', '0'),
-    ('1024', 'Steve', 'Walton', '1375', '1801 Baltimore Ave', 'Kansas City', 'MO', 64108, '8161329745', '1'),
-    ('1045', 'Barbara', 'Smith', '2374', '16919 Audrey St', 'Omaha', 'NE', 68136, '4021269604', '0'),
-    ('1064', 'Patricia', 'White', '1728', '1512 W Berwyn Ave', 'Chicago', 'IL', 60640, '3121216247', '1'),
-    ('1086', 'Robert', 'Thomas', '1001', '110 E Water St', 'Decorah', 'IA', 52101, '5151445162', '1'),
-    ('1091', 'Robert', 'White', '1003', '2300 Muscatine Ave', 'Iowa City', 'IA', '0000', '5151459846', '0'),
-    ('1041', 'William', 'Johnson', '2376', '1101 S 119th St', 'Omaha', 'NE', 68144, '4021015829', '1'),
-    ('1029', 'David', 'Rodriguez', '1835', '9747 E 21st St N', 'Wichita', 'KS', 67206, '9131415414', '0');
+    ('1007', 'Maria', 'Rodriguez', '1000', '6541 Hollywood Blvd', 'Los Angeles', 'CA', '90028',    '2131092912', '0'),
+    ('1024', 'Steve', 'Walton', '1001', '1801 Baltimore Ave', 'Kansas City', 'MO', 64108, '8161329745', '1'),
+    ('1045', 'Barbara', 'Smith', '1008', '16919 Audrey St', 'Omaha', 'NE', 68136, '4021269604', '0'),
+    ('1064', 'Patricia', 'White', '1013', '1512 W Berwyn Ave', 'Chicago', 'IL', 60640, '3121216247', '1'),
+    ('1086', 'Robert', 'Thomas', '1020', '110 E Water St', 'Decorah', 'IA', 52101, '5151445162', '1'),
+    ('1091', 'Robert', 'White', '1022', '2300 Muscatine Ave', 'Iowa City', 'IA', '0000', '5151459846', '0'),
+    ('1041', 'William', 'Johnson', '1006', '1101 S 119th St', 'Omaha', 'NE', 68144, '4021015829', '1'),
+    ('1029', 'David', 'Rodriguez', '1003', '9747 E 21st St N', 'Wichita', 'KS', 67206, '9131415414', '0');
+
 
     
 INSERT INTO products
@@ -484,56 +488,57 @@ INSERT INTO shippingMethods
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1001', '1122', '2024-11-02', '1007', '1003', 
+    VALUES('1001', '1000', '2024-11-02', '1007', '1003', 
     '2024-12-02', '2024-11-16', '1001');
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1002', '2376', '2024-11-02', '1041', '1007',
+    VALUES('1002', '1006', '2024-11-02', '1041', '1007',
     '2024-12-02', NULL, NULL); -- Phone Assistance
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1003', '1835', '2024-11-03', '1029', '1007', 
+    VALUES('1003', '1003', '2024-11-03', '1029', '1007', 
     '2024-12-02', '2024-11-17', '1002');
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1004', '1375', '2024-11-03', '1024', '1007',
+    VALUES('1004', '1001', '2024-11-03', '1024', '1007',
     '2024-12-03', '2024-11-17', '1003');
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1005', '2374', '2024-11-09', '1045', '1007', 
+    VALUES('1005', '1008', '2024-11-09', '1045', '1007', 
     '2024-12-09', '2024-11-23', '1004');
     
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1006', '3724', '2024-11-18', '1032', '1007', 
+    VALUES('1006', '1004', '2024-11-18', '1032', '1007', 
     '2024-12-18', '2024-11-30', '1001');
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1007', '1728', '2024-11-20', '1064', '1004', 
+    VALUES('1007', '1013', '2024-11-20', '1064', '1004', 
     '2024-12-20', NULL, NULL); -- Phone assistance
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1008', '1001', '2024-11-20', '1086', '1006', 
+    VALUES('1008', '1020', '2024-11-20', '1086', '1006', 
     '2024-12-20', '2024-12-04', '1005');
 
 INSERT INTO orders
 	(orderID, customerID, orderDate, contactID, employeeID, 
     paymentDueDate, projectedDeliveryDate, methodID)
-    VALUES('1009', '1003', '2024-11-28', '1091', '1006', 
+    VALUES('1009', '1022', '2024-11-28', '1091', '1006', 
     '2024-12-28', '2024-12-12', '1006');
+
     
 -- order details
 INSERT INTO orderDetails
